91692cce85f778b14567a0b95e3087f2bc4b438c
Marcelo Oliveira <santos.marcelooliveira@gmai.com>
2020-09-09 20:36:12 -0300

    main dinamic

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,7 +2,7 @@
 var xhr = new XMLHttpRequest();
 var link1 = document.querySelector('.link1')
 link1.addEventListener('click', ()=>{
-    xhr.open('get', 'http://192.168.42.96:8080/views/historic.html', true);
+    xhr.open('get', 'http://192.168.0.64:8080/views/historic.html', true);
     xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) { 
         document.querySelector("main").innerHTML = xhr.responseText;
