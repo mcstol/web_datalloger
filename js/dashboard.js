e6d525990c9e12f5efabe8bb5b65ac2a023132fc
Marcelo Oliveira <santos.marcelooliveira@gmail.com>
2020-10-15 23:09:00 -0300

    post json formulario

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-
+var senhasIguais = false
 const dominio = 'http://192.168.0.64:8080'
 
 function atualizaMain(link) {
@@ -26,7 +26,30 @@ document.querySelector('.link4')
 
 
 
+function confirmaSenha(event) {
+    console.log("acessou a função")
+    var senha = $("#senha").val()
+    var confirmaSenha = $("#confirmeSenha").val()
+    if (senha !== confirmaSenha) {
+        alert("Senhas não são iguais")
+        return false
+    } else {
+        senhasIguais = true
+    }
+}
 
 function save() {
-    console.log("clicou o botão!")
+    console.log("clicou o botão!", senhasIguais)
+    var formulario = JSON.stringify({
+        email: $("#email1").val(),
+        senha: $("#senha").val()
+    })
+    console.log(formulario)
+    if (senhasIguais == true) {
+        xhr.open("POST", "http://localhost:3000", true)
+        xhr.setRequestHeader("content-type", "application/json")
+        xhr.send(formulario)
+    } else {
+        alert("Senhas não são iguais!")
+    }
 }
